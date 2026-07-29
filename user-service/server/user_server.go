d190b00dd442f199f75019a04b0635233175912f
farkhanisturkia <farkhan22032000@gmail.com>
2026-07-29 11:58:33 +0700

    [update] modification all function with role and password added, create GetUserByEmailInternal function

--- user-service/server/user_server.go
+++ user-service/server/user_server.go
@@ -9,6 +9,7 @@ import (
 	"user-service/pb/pbconnect"
 
 	"connectrpc.com/connect"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserServer struct {
@@ -17,33 +18,46 @@ type UserServer struct {
 	EducationClient pbconnect.EducationServiceClient
 }
 
-// 1. CreateUser
 func (s *UserServer) CreateUser(
 	ctx context.Context,
 	req *connect.Request[pb.CreateUserRequest],
 ) (*connect.Response[pb.User], error) {
-	query := `INSERT INTO users (name, email) VALUES (?, ?)`
-	res, err := s.DB.ExecContext(ctx, query, req.Msg.Name, req.Msg.Email)
-	if err != nil {
-		return nil, connect.NewError(connect.CodeInternal, err)
-	}
+	role := req.Msg.Role
+    if role != "admin" && role != "user" {
+        role = "user"
+    }
+
+	var hashedPassword string
+    if req.Msg.Password != "" {
+        hash, err := bcrypt.GenerateFromPassword([]byte(req.Msg.Password), bcrypt.DefaultCost)
+        if err != nil {
+            return nil, connect.NewError(connect.CodeInternal, fmt.Errorf("gagal hash password: %w", err))
+        }
+        hashedPassword = string(hash)
+    }
+
+	query := `INSERT INTO users (name, email, password, role) VALUES (?, ?, ?, ?)`
+    res, err := s.DB.ExecContext(ctx, query, req.Msg.Name, req.Msg.Email, hashedPassword, role)
+    if err != nil {
+        return nil, connect.NewError(connect.CodeInternal, err)
+    }
 
 	id, _ := res.LastInsertId()
 	return connect.NewResponse(&pb.User{
 		Id:    id,
 		Name:  req.Msg.Name,
 		Email: req.Msg.Email,
+		Role:  role,
 	}), nil
 }
 
-// 2. GetUser (Ambil data user + List job-nya dari Job Service + List Education-nya dari Education Service)
 func (s *UserServer) GetUser(
 	ctx context.Context,
 	req *connect.Request[pb.GetUserRequest],
 ) (*connect.Response[pb.User], error) {
 	var user pb.User
-	query := `SELECT id, name, email FROM users WHERE id = ?`
-	err := s.DB.QueryRowContext(ctx, query, req.Msg.Id).Scan(&user.Id, &user.Name, &user.Email)
+	query := `SELECT id, name, email, role FROM users WHERE id = ?`
+	err := s.DB.QueryRowContext(ctx, query, req.Msg.Id).Scan(&user.Id, &user.Name, &user.Email, &user.Role)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return nil, connect.NewError(connect.CodeNotFound, fmt.Errorf("user tidak ditemukan"))
@@ -51,7 +65,6 @@ func (s *UserServer) GetUser(
 		return nil, connect.NewError(connect.CodeInternal, err)
 	}
 
-	// Panggil Job Service via Connect RPC Client
 	jobRes, err := s.JobClient.GetJobsByUser(ctx, connect.NewRequest(&pb.GetJobsByUserRequest{
 		UserId: user.Id,
 	}))
@@ -59,7 +72,6 @@ func (s *UserServer) GetUser(
 		user.Jobs = jobRes.Msg.Jobs
 	}
 
-	// Panggil Education Service via Connect RPC Client
 	educationRes, err := s.EducationClient.GetEducationsByUser(ctx, connect.NewRequest(&pb.GetEducationsByUserRequest{
 		UserId: user.Id,
 	}))
@@ -70,37 +82,49 @@ func (s *UserServer) GetUser(
 	return connect.NewResponse(&user), nil
 }
 
-// 3. UpdateUser
+func (s *UserServer) GetUserByEmailInternal(
+	ctx context.Context,
+	req *connect.Request[pb.GetUserByEmailRequest],
+) (*connect.Response[pb.UserInternal], error) {
+	var userInternal pb.UserInternal
+	query := `SELECT id, name, email, role, password FROM users WHERE email = ?`
+	err := s.DB.QueryRowContext(ctx, query, req.Msg.Email).Scan(&userInternal.Id, &userInternal.Name, &userInternal.Email, &userInternal.Role, &userInternal.Password)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, connect.NewError(connect.CodeNotFound, fmt.Errorf("user tidak ditemukan"))
+		}
+		return nil, connect.NewError(connect.CodeInternal, err)
+	}
+
+	return connect.NewResponse(&userInternal), nil
+}
+
 func (s *UserServer) UpdateUser(
 	ctx context.Context,
 	req *connect.Request[pb.UpdateUserRequest],
 ) (*connect.Response[pb.User], error) {
-	query := `UPDATE users SET name = ?, email = ? WHERE id = ?`
-	_, err := s.DB.ExecContext(ctx, query, req.Msg.Name, req.Msg.Email, req.Msg.Id)
+	query := `UPDATE users SET name = ?, email = ?, role = ? WHERE id = ?`
+	_, err := s.DB.ExecContext(ctx, query, req.Msg.Name, req.Msg.Email, req.Msg.Role, req.Msg.Id)
 	if err != nil {
 		return nil, connect.NewError(connect.CodeInternal, err)
 	}
 	return s.GetUser(ctx, connect.NewRequest(&pb.GetUserRequest{Id: req.Msg.Id}))
 }
 
-// 4. DeleteUser (Hapus User + Cascade Hapus Job terkait via Job Service + Cascade Hapus Education terkait via Education Service)
 func (s *UserServer) DeleteUser(
 	ctx context.Context,
 	req *connect.Request[pb.DeleteUserRequest],
 ) (*connect.Response[pb.EmptyUserResponse], error) {
-	// Hapus user dari DB local
 	query := `DELETE FROM users WHERE id = ?`
 	_, err := s.DB.ExecContext(ctx, query, req.Msg.Id)
 	if err != nil {
 		return nil, connect.NewError(connect.CodeInternal, err)
 	}
 
-	// Hapus seluruh job terkait di Job Service
 	_, _ = s.JobClient.DeleteJobsByUser(ctx, connect.NewRequest(&pb.DeleteJobsByUserRequest{
 		UserId: req.Msg.Id,
 	}))
 
-	// Hapus seluruh education terkait di Education Service
 	_, _ = s.EducationClient.DeleteEducationsByUser(ctx, connect.NewRequest(&pb.DeleteEducationsByUserRequest{
 		UserId: req.Msg.Id,
 	}))
